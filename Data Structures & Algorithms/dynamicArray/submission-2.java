604d13f3b0bd3d90632559a24b57879940424352
skarasik26 <skarasik26@gmail.com>
2026-06-15 13:53:31 -0400

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/dynamicArray/submission-2.java
@@ -0,0 +1,48 @@
+class DynamicArray {
+    private int[] arr;
+    private int size;
+    private int capacity;
+
+    public DynamicArray(int capacity) {
+        arr = new int[capacity];
+        this.capacity = capacity;
+        size = 0;
+    }
+
+    public int get(int i) {
+        return arr[i];
+    }
+
+    public void set(int i, int n) {
+        arr[i] = n;
+    }
+
+    public void pushback(int n) {
+        if (size == capacity){
+            resize();
+        }
+        arr[size++] = n;
+    }
+
+    public int popback() {
+        return arr[--size];
+    }
+
+    private void resize() {
+        capacity *= 2;
+        int[] temp = new int[capacity];
+        for (int i = 0; i < size; i++){
+            temp[i] = arr[i];
+        }
+        arr = temp;
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
