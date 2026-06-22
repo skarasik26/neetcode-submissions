116f2f4961eca6364d9db7ab1a6202f02e8cdfe6
skarasik26 <skarasik26@gmail.com>
2026-06-22 15:44:44 -0400

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        if(lists.length == 1) return lists[0];
+        int m = lists.length/2;
+        ListNode left = mergeKLists(Arrays.copyOfRange(lists, 0, m));
+        ListNode right = mergeKLists(Arrays.copyOfRange(lists, m, lists.length));
+        return merge(left, right);
+    }
+
+    public ListNode merge (ListNode leftHead, ListNode rightHead){
+        if(leftHead == null && rightHead == null) return null;
+        else if (leftHead == null) return rightHead;
+        else if (rightHead == null) return leftHead;
+        ListNode res = null;
+        if (leftHead.val <= rightHead.val) {
+            res = leftHead;
+            leftHead = leftHead.next;
+        } else{
+            res = rightHead;
+            rightHead = rightHead.next;
+        }
+        ListNode cur = res;
+        while(leftHead != null && rightHead != null){
+            if(leftHead.val <= rightHead.val){
+                cur.next = leftHead;
+                cur = cur.next;
+                leftHead = leftHead.next;
+            }
+            else{
+                cur.next = rightHead;
+                cur = cur.next;
+                rightHead = rightHead.next;
+            }
+        }
+        while(leftHead != null){
+            cur.next = leftHead;
+            cur = cur.next;
+            leftHead = leftHead.next;
+        }
+
+        while(rightHead != null){
+            cur.next = rightHead;
+            cur = cur.next;
+            rightHead = rightHead.next;
+        }
+        return res;
+    }
+}
