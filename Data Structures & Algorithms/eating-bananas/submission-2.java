afdf3363c36a939d9ca057381f0c040e109a98f2
skarasik26 <skarasik26@gmail.com>
2026-06-23 14:07:27 -0400

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1;
+        int r = 0;
+        for (int pile : piles) {
+            r = Math.max(r, pile);
+        }
+        int m;
+        int currMin = 0;
+        while(l <= r){
+            m = l + (r-l)/2;
+            if(correct(piles, h, m)){
+                currMin = m;
+                r = m - 1;
+            } else{
+                l = m + 1;
+            }
+        }
+        return currMin;
+    }
+    public boolean correct(int[] piles, int h, int guess){
+        long num = 0;
+        for(int i = 0; i<piles.length; i++){
+            num+=(piles[i] + guess - 1)/guess;
+        }
+        return num <= h;
+    }
+}
