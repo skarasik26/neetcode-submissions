ac9a89965815d7374a4b391b7d3b2d5b52e0fa02
skarasik26 <skarasik26@gmail.com>
2026-06-25 13:08:44 -0400

    Add: delete-node-in-a-bst - submission-1

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-1.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return null;
+
+        if(root.val > key){
+            root.left = deleteNode(root.left, key);
+        } else if(root.val < key){
+            root.right = deleteNode(root.right, key);
+        } else{
+            if(root.left == null){
+                root = root.right;
+            } else if(root.right == null){
+                root = root.left;
+            }else{
+                TreeNode min = findMin(root.right);
+                root.val = min.val;
+                root.right = deleteNode(root.right, min.val);
+            }
+        }
+        return root;
+    }
+
+    public TreeNode findMin(TreeNode root){
+        while (root.left != null) root = root.left;
+        return root;
+    }
+}
